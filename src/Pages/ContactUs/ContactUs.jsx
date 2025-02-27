b64b8f1ae92c6c22a7f354336838296e866e4033
huda1197 <82894112+huda1197@users.noreply.github.com>
2025-02-27 14:13:49 +0300

    last

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -8,45 +8,45 @@ function ContactUs(){
     return (
         <>
 <Navbar/>
-<h2 class='header1'>Contact Us</h2>
-<div class="footer">
-<h2 CLASS="projects_header"></h2>
-<div class='trianglebox'>
-<form class="my_form">
-<div class="row">
-<div class="col-25">
-<label for="fname">Name</label>
+<h2 className='header1'>Contact Us</h2>
+<div className="footer">
+<h2 className="projects_header"></h2>
+<div className='trianglebox'>
+<form className="my_form">
+<div className="row">
+<div className="col-25">
+<label htmlFor="fname">Name</label>
 </div>
-<div class="col-75">
-<input type="text" id="fname" name="name" placeholder="Your name.."/>
-</div>
-</div>
-<div class="row">
-<div class="col-25">
-<label for="lname">Email</label>
-</div>
-<div class="col-75">
-<input type="email" id="lname" name="Email" placeholder="Your Email.."/>
+<div className="col-75">
+<input type="text" id="fname" name="name" className="input-field" placeholder="Your name.."/>
 </div>
 </div>
-<div class="row">
-<div class="col-25">
-<label for="country">Number</label>
+<div className="row">
+<div className="col-25">
+<label htmlFor="lname">Email</label>
 </div>
-<div class="col-75">
-<input type="number" id="lname" name="number" placeholder="number.."/>
+<div className="col-75">
+<input type="email" id="lname" name="Email" className="input-field" placeholder="Your Email.."/>
 </div>
 </div>
-<div class="row">
-<div class="col-25">
-<label for="subject">Subject</label>
+<div className="row">
+<div className="col-25">
+<label htmlFor="country">Number</label>
 </div>
-<div class="col-75">
+<div className="col-75">
+<input type="number" id="lname" name="number" className="input-field" placeholder="number.."/>
+</div>
+</div>
+<div className="row">
+<div className="col-25">
+<label htmlFor="subject">Subject</label>
+</div>
+<div className="col-75">
 <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
 </div>
 </div>
 <br/>
-<div class="row">
+<div className="row">
 <input type="submit" value="Submit"/>
 </div>
 </form>
